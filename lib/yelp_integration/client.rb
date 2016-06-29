de83c26bb71461583353d45db8d7fe4c9430c9c5
ru7dy <rudymelody@gmail.com>
2016-06-29 15:40:34 -0700

    add yelp query integration

---
+++ lib/yelp_integration/client.rb
@@ -0,0 +1,23 @@
+#https://github.com/Yelp/yelp-ruby
+require 'yelp'
+
+module YelpIntegration
+	class Client
+
+		def self.query(city, params)
+			client = Yelp::Client.new({ consumer_key: KH7e0YJNALkAqjch2X3r-g,
+                         					consumer_secret: J6Lu5DBor5HQHT4VSipFqm6LRVc,
+                         					token: nzRtw0qgDXqW6D6Nc8gDZOmMqpBaPCtX,
+                         					token_secret: Ti0kfu0begnJglPCaK-CpUvov8s
+																})
+			city ||= 'San Francisco'
+			params[:term] ||= 'food'
+			params[:limit] ||= 5
+			response = client.search(city, params)
+			response.businesses.join(',')
+		end
+
+	end
+end
+
+
